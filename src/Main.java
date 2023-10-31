f9006506a93496383ad35aeffdf3b752c7b11361
besher AL ADDAM <beshoux@outlook.fr>
2023-10-31 15:44:21 +0100

    Gym Fonctionnel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Gym nantesGym = new Gym(5, new HashMap<>() {
+            {
+                put(MachineType.LEGPRESSMACHINE, 5);
+                put(MachineType.HALTERE, 5);
+                put(MachineType.SQUATMACHINE, 5);
+                put(MachineType.LEGEXTENSIONMACHINE, 5);
+                put(MachineType.LEGCURLMACHINE, 5);
+                put(MachineType.LATPULLDOWNMACHINE, 5);
+                put(MachineType.POULIEVISAVIS, 5);
+            }
+        });
+
+        nantesGym.openAjd();
     }
 }
